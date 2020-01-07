78b2582be681bff1a9ef0fbde994c2726ac9988c
OZOAR <vadimrezunik@gmail.com>
2020-01-07 19:35:12 +0300

    first commit

---
+++ js/navdrop.js
@@ -0,0 +1,9 @@
+$(window).resize(function () {
+            var width = $(window).width();
+            if (width > 1200) {
+                var width = $(window).width(); if (width > 1200) {
+                    document.getElementById("navbarDropdownMenuLink").onclick = "location.href = 'catalog.html'"
+
+                }
+            }
+        });
